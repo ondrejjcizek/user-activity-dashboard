c3262a6b03feb782050898f5047d1fdd2c66c804
Ondrej Cizek <ondrejj.cizek@gmail.com>
2025-04-25 16:53:54 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import '../app.css';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	const { children }: Props = $props();
+</script>
+
+<main>
+	{@render children()}
+</main>
